83850bb2ca44ba18dfb58b224e0cfa214c5e1e77
Asim <asim@riseup.net>
2019-08-09 20:33:17 -0700

    remove file format from being written when creating a new file

--- ffmpeg.py
+++ ffmpeg.py
@@ -4,4 +4,5 @@ import subprocess
 all_trailer_files = glob.glob('trailer_test/*')
 
 for movie in all_trailer_files:
-    subprocess.run(['ffmpeg', '-i', movie, '-vf','fps=1, scale=299:299', movie+"%04d.png"])
+    movie_with_no_fileend = movie.split('.')
+    subprocess.run(['ffmpeg', '-i', movie, '-vf','fps=1, scale=299:299', movie_with_no_fileend[0]+"%04d.png"])
